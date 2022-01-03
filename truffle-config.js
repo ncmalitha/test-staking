989729928bebbb01d23f156ace4a3cecf59b3b37
ncmalitha <ncmalitha@gmail.com>
2022-01-04 00:51:51 +0100

    adding solidity contract version

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   networks: {
@@ -8,11 +9,23 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    gorli: {
+      host: "https://goerli.infura.io/v3/427450e00fb34113a7eabb9c350fd44d",
+      port: 80,
+      network_id: "5" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/<INFURA_PROJECT_ID>")
+      },
+      network_id: 3
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version:"^0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
