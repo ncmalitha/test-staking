718c4eb9133fedab9807d94084dfc94da6049c0c
ncmalitha <ncmalitha@gmail.com>
2022-02-06 23:46:25 +0100

    adding block based rewards

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -16,10 +16,15 @@ contract TokenFarm is Ownable, Pausable {
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
 
+    mapping(address => uint) public dappTokenBalance;
+    uint public interestRate;
+    uint public lastUpdateTime; // last block updated time  
+    uint constant secondsPerYear = 31556952;
 
     constructor (DappToken _dappToken, DaiToken _daiToken) {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        lastUpdateTime = block.timestamp;
     }
 
     function stakeTokens(uint _amount) public whenNotPaused {
@@ -45,16 +50,43 @@ contract TokenFarm is Ownable, Pausable {
         isStaking[msg.sender] = false;
     }
 
-    function issueTokens() public onlyOwner {
+    function issueTokens() public {
+        if(getTimeGapSinceLastBlock() == 0) {
+            return;
+        }
+        lastUpdateTime = block.timestamp; // update timestamp
+        //https://solidity-by-example.org/defi/staking-rewards/
         for(uint i = 0; i  < stakers.length; i++) {
             address receipient = stakers[i];
             uint balance = stakingBalance[receipient];  
 
             if (balance > 0) {
-                dappToken.transfer(receipient, balance);
+                dappTokenBalance[msg.sender] += rewardPerAmount(balance);
             }
-
         }
+    }
+
+    function getTimeGapSinceLastBlock() public view returns (uint) {
+        return (lastUpdateTime - block.timestamp);
+    } 
+
+    function rewardPerAmount(uint _amount) public view returns (uint) {
+        if(_amount == 0) {
+            return 0;
+        }
+        return _amount * (interestRate / 100) * (getTimeGapSinceLastBlock() / secondsPerYear);
+    }
+
+    function claimRewards() public {
+        uint balance = dappTokenBalance[msg.sender];  
+        require(balance > 0 && dappTokenBalance[msg.sender] >= balance, "claim amount has to greater than zero ");
+        dappToken.transfer(msg.sender, balance);
+
+        dappTokenBalance[msg.sender] -= balance;
+    }
+
+    function setInterest(uint _interestRate) public onlyOwner {
+        interestRate = _interestRate;
     }
 
     function pause() public onlyOwner {
